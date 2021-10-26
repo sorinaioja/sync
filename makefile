9ff485e28881ec0a0be3c0f4403b06550e8b6fdc
sorinaioja <sorina_ioja@yahoo.com>
2021-10-26 14:51:07 +0300

    problem 1

---
+++ makefile
@@ -0,0 +1,2 @@
+write: src/ex1.c
+	gcc -o build src/ex1.c -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -lglib-2.0
---
+++ src/ex1.c
@@ -0,0 +1,37 @@
+#include "../include.h"
+
+int counter;
+GThread *tid1;
+GThread *tid2;
+GMutex lock;
+
+void *func(void* arg){
+
+    g_mutex_lock(&lock);
+
+    counter+=1;
+    printf("Task %d has started\n", counter);
+
+    printf("Task %d has finished\n", counter);
+
+    g_mutex_unlock(&lock);
+    return NULL;
+}
+
+int main(){
+
+    printf("Hello world\n");
+
+    g_mutex_init(&lock);
+
+
+    tid1 = g_thread_new(NULL, &func, NULL);
+    tid2 = g_thread_new(NULL, &func, NULL);
+
+
+    g_thread_join(tid1);
+    g_thread_join(tid2);
+
+    //g_mutex_destroy(&lock);
+    return 0;
+}
